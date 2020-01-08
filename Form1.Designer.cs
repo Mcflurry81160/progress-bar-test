e506d267a1f079100c583766bf06f6ed304eb645
kmannapperuma <kmannapperuma@machship.com>
2020-01-09 04:24:29 +1100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace progress_bar_test_3_dnf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartProcess = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStartProcess
+            // 
+            this.btnStartProcess.Location = new System.Drawing.Point(173, 100);
+            this.btnStartProcess.Name = "btnStartProcess";
+            this.btnStartProcess.Size = new System.Drawing.Size(133, 23);
+            this.btnStartProcess.TabIndex = 0;
+            this.btnStartProcess.Text = "Start process";
+            this.btnStartProcess.UseVisualStyleBackColor = true;
+            this.btnStartProcess.Click += new System.EventHandler(this.StartProcess_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(173, 168);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(229, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 327);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStartProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartProcess;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progress_bar_test_3_dnf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Visible = false;
+        }
+
+        private void StartProcess_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            btnStartProcess.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 50;
+
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork;
+            bw.RunWorkerCompleted += bw_RunWorkerCompleted;
+            bw.RunWorkerAsync();
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // INSERT TIME CONSUMING OPERATIONS HERE
+            // THAT DON'T REPORT PROGRESS
+            Thread.Sleep(10000);
+        }
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.MarqueeAnimationSpeed = 0;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Value = progressBar1.Minimum;
+
+            btnStartProcess.Enabled = true;
+            progressBar1.Visible = false;
+            MessageBox.Show("Done!");
+        }
+    }
+}
